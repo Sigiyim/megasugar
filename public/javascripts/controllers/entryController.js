7aa18b9cc25c3dc8b71f9e75b48dbc297e45ad2f
unknown <cgm18164@2LB2HQ1.mfc.corp.mckee.com>
2014-01-14 14:29:44 -0500

    Adding controls to the entry page.

--- public/javascripts/controllers/entryController.js
+++ public/javascripts/controllers/entryController.js
@@ -1,7 +1,15 @@
 angular.module('EntryController', [])
 .controller('EntryController', [
     '$scope',
-    function($scope) {
+    '$filter',
+    function($scope, $filter) {
+        $scope.reading = {};
+        var dateFilter = $filter('date');
 
+        $scope.reading.date = new Date().toISOString().substr(0, 10);
+        $scope.reading.time = dateFilter(new Date(), 'HH:mm');
+        $scope.reading.sugar = 0;
+        $scope.reading.unitsOfInsulin = 0;
+//        $scope.reading.typeOfInsulin = '';
     }
 ]);
